33b1044754b587d56aa8cb433e76c98a402dc428
Aadarsh Thakur <aadarshthakur80501@gmail.com>
2022-04-09 14:07:03 +0530

    birthday reminder

---
+++ birthday-reminder/src/Birthday.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { data } from "./data";
+
+const Birthday = () => {
+    const [people, setPeople] = React.useState(data)
+
+    const removeUser = (id) => {
+        const newPeople = people.filter((person) => person.id != id)
+        setPeople(newPeople)
+    }
+    return <>
+        <h1>{people.length} birthdays today</h1>
+        {people.map((person) => {
+            const {id, name, age, img} = person
+
+            return <div className="items" >
+                <div className="item" >
+                    <div className="user" key={id}>
+                        <img src={img} alt="" />
+                        <div className="info">
+                            <h2>{name}</h2>
+                            <p>{age}</p>
+                        </div>
+                    </div>
+                    <a href="#" className="remove-btn" onClick={() => removeUser(id)}>remove</a>
+                </div>
+            </div>
+        })}
+
+        <a href="#" className="btn" onClick={() => setPeople([])}>clear all</a>
+
+    </>
+
+}
+
+export default Birthday;
---
+++ birthday-reminder/src/data.js
@@ -0,0 +1,7 @@
+export const data = [
+    { id: 1, name: "Bertie Yates", age: "29 years", img: "https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"},
+    { id: 2, name: "Hester Hogan", age: "32 years", img: "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"},
+    { id: 3, name: "Larry Little", age: "36 years", img: "https://images.unsplash.com/photo-1616325629936-99a9013c29c6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80"},
+    { id: 4, name: "Sean Walsh", age: "34 years", img: "https://images.unsplash.com/photo-1619443492967-f04dfa229b14?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"},
+    { id: 5, name: "Lola Gardner", age: "29 years", img: "https://images.unsplash.com/photo-1621398944643-f994c56c8f54?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80"}
+]
